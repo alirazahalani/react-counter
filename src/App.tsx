d5c04e8b9a60f1624c32ad3127aec3307ea0d05a
alirazahalani <alirazahalani@gmail.com>
2023-03-17 19:48:09 +0400

    react-blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  function handleClick(amt: number) {
-    setCount((p) => (p += amt))
-  }
 
   return (
     <div className='App'>
       <>
-        <h1>Hello Counter</h1>
-        <div className='wrapper'>
-          <button onClick={() => handleClick(-1)}>
-            <span className='material-icons'>remove</span>
-          </button>
-          <p>{count}</p>
-          <button onClick={() => handleClick(1)}>
-            <span className='material-icons'>add</span>
-          </button>
-        </div>
+      <h1>App Component</h1>
       </>
     </div>
   )
